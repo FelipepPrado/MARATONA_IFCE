df37ccf93def4eaf689e2d0de2f2264239ba3e27
FelipepPrado <felipepdelimaa@gmail.com>
2024-12-01 02:04:12 -0300

    Update C.cpp

--- Semana 1/C.cpp
+++ Semana 1/C.cpp
@@ -3,23 +3,19 @@ using namespace std;
 
 int main() {
     int i;
-    int pos1, pos2;
+    int pos1;
     int vetor[100];
     cin >> vetor[0];
     pos1 = 0;
-    pos2 = 0;
-    for(i = 1 ; i < 5; i++){
+    for(i = 1 ; i < 100; i++){
         
         cin >> vetor[i];
         
         if(vetor[pos1] < vetor[i]){
             pos1 = i;
         }
-        if(vetor[pos2] > vetor[i]){
-            pos2 = i;
-        }
     }
     cout << vetor[pos1] << endl;
-    cout << vetor[pos2] << endl
+    cout << pos1 << endl;
     return 0;
 }
